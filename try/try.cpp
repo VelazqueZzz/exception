8398f8dd4b38dcc36580feb9ab988a870500b8cb
ITaly177 Milan <patrokl2010@yandex.ru>
2023-02-15 16:38:21 +0300

    Add project files.

---
+++ try/try.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+float f(float x)
+{
+	if (x == 0) throw 1;
+	return 1 / x;
+}
+
+int main(void)
+{
+	float x;
+	cout << "x? ";
+	cin >> x;
+	try 
+	{
+		cout << "1/x =" << f(x) << endl;
+	}
+	catch(int &e)
+	{
+		cout << "Division on zero exception" << endl;
+	}
+	
+	return 0;
+}
+
+
